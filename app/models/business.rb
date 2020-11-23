507c867640b35773d3543645abd1bab99d7d28d9
Lukas Horstmann <lukas.horstmann.code@gmail.com>
2020-11-23 16:59:40 +0100

    Validations done

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,9 @@
 class Business < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :bookmarks
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :short_bio, presence: true
 end
