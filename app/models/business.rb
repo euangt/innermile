1d3548967e5a0f2fc28969d891aa92de50b9e0e3
Ben Gordon <benharrisgordon@gmail.com>
2020-11-24 11:23:53 +0100

    Added Gemfile, updated Business and User Models

--- app/models/business.rb
+++ app/models/business.rb
@@ -6,4 +6,7 @@ class Business < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :short_bio, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
