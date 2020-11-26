b8a1a57a3c867687b8c8cc5b163fe32953b2da98
Yoko Kasai <ykookaasi@gmail.com>
2020-11-26 12:25:02 +0100

    Change validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :business
 
-  validates :content, presence: true, length: { minimum: 15 }
-  validates :post_image, presence: true
+  validates :content, presence: true, length: { minimum: 5 }
 
   has_one_attached :post_image
 end
