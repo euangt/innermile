dba1e092225c586c9f19b00c71df885e13a6176c
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-11-30 11:17:06 +0100

    name added to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_095938) do
+ActiveRecord::Schema.define(version: 2020_11_30_101445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_095938) do
     t.float "latitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "event_name"
     t.index ["business_id"], name: "index_events_on_business_id"
   end
 
