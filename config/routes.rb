b83816dd90022eae7ff152e385745dbe2be3c076
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-11-30 16:56:09 +0100

    routes unduplicated

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :businesses do
-    resources :bookmarks, only: [ :create ]
-  end
   resources :users, only: [ :edit, :update ] do
     resources :bookmarks, only: [ :index,  :destroy ]
   end
   resources :businesses do
     resources :posts, only: [:create, :destroy, :edit, :update]
     resources :events, only: [:show, :new, :create, :edit, :update, :destroy ]
+    resources :bookmarks, only: [ :create ]
   end
   resources :bookmarks, only: [ :destroy ]
   resources :posts, only: [ :index ]
