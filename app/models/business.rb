031cd04f223c3c409fe3cca668f003601ec0d142
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-11-30 14:55:10 +0100

    initial event card

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,6 +3,7 @@ class Business < ApplicationRecord
   belongs_to :category
   has_many :bookmarks, dependent: :destroy
   has_many :posts, dependent: :destroy
+  has_many :events, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
