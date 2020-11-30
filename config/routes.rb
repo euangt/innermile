153d69f9ec38556f4985078495f37916d04c5fe1
Yoko Kasai <ykookaasi@gmail.com>
2020-11-30 18:14:53 +0100

    Take extra routes out

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :businesses do
-    resources :bookmarks, only: [ :create ]
-  end
   resources :bookmarks, only: [ :index,  :destroy ]
-  resources :users, only: [ :edit, :update ] 
+  resources :users, only: [ :edit, :update ]
   resources :businesses do
     resources :posts, only: [:create, :destroy, :edit, :update]
     resources :events, only: [:show, :new, :create, :edit, :update, :destroy ]
