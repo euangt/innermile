49989970af5735c0c7c80684285e857db6e9a763
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-12-01 14:39:43 +0100

    event routing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -34,7 +34,7 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     authorize @event
     if @event.update(event_params)
-      redirect_to business_path(@business)
+      redirect_to business_event_path(@event.business, @event)
     else
       render :edit
     end
