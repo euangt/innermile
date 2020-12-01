873a159bf8e9af22cba3a27a0546f5b0987703aa
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-12-01 12:28:31 +0100

    meta_title throwing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
   include Pundit
 
   # Pundit: white-list approach.
@@ -13,9 +16,6 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
-  def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
-  end
 
   private
 
