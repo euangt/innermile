4d8a2426deeefc3d0538cc7e2079f7f82e506eab
Euan Gillespie-Taylor <euan.gillespie@outlook.com>
2020-12-01 14:25:10 +0100

    without meta image tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+  
   include Pundit
 
   # Pundit: white-list approach.
