046fae64be67f003ceb84bbbedd9edec80c9fe2c
Lukas Horstmann <lukas.horstmann.code@gmail.com>
2020-12-01 11:38:21 +0100

    Functionality implemented

--- app/policies/conversation_policy.rb
+++ app/policies/conversation_policy.rb
@@ -1,4 +1,9 @@
 class ConversationPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
   def show?
     user == record.user
     business == record.business.user
--- app/policies/message_policy.rb
+++ app/policies/message_policy.rb
@@ -1,5 +1,8 @@
 class BookmarkPolicy < ApplicationPolicy
   class Scope < Scope
+    def resolve
+      scope.all
+    end
     def create?
       user == record.user
     end
