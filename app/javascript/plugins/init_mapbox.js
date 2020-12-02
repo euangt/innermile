33819f049e593a56d9c321d5ce81f7f0eb0f3e25
Ben Gordon <benharrisgordon@gmail.com>
2020-12-02 17:03:16 +0100

    Added distance calculation to map markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -105,98 +105,98 @@ const initMapbox = () => {
 
     fitMapToMarkers(map, businessMarkers);
 
-    getRoute([13.4004488, 52.5314138]);
-    // create a function to make a directions request
-    function getRoute(end) {
-      // make a directions request using cycling profile
-      // an arbitrary start will always be the same
-      // only the end or destination will change
-      var start = [homeMarker.lng, homeMarker.lat]; // Had to move this outside of the getRoute function in order to make the getRoute(start) work on line 146
-      var url = 'https://api.mapbox.com/directions/v5/mapbox/walking/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+    // getRoute([13.4004488, 52.5314138]);
+    // // create a function to make a directions request
+    // function getRoute(end) {
+    //   // make a directions request using cycling profile
+    //   // an arbitrary start will always be the same
+    //   // only the end or destination will change
+    //   var start = [homeMarker.lng, homeMarker.lat]; // Had to move this outside of the getRoute function in order to make the getRoute(start) work on line 146
+    //   var url = 'https://api.mapbox.com/directions/v5/mapbox/walking/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
 
-      // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-      var req = new XMLHttpRequest();
-      req.open('GET', url, true);
-      req.onload = function() {
-        var json = JSON.parse(req.response);
-        console.log(json);
-        var data = json.routes[0];
-        var route = data.geometry.coordinates;
-        var geojson = {
-          type: 'Feature',
-          properties: {},
-          geometry: {
-            type: 'LineString',
-            coordinates: route
-          }
-        };
-        // if the route already exists on the map, reset it using setData
-        if (map.getSource('route')) {
-          map.getSource('route').setData(geojson);
-        } else { // otherwise, make a new request
-          map.addLayer({
-            id: 'route',
-            type: 'line',
-            source: {
-              type: 'geojson',
-              data: {
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                  type: 'LineString',
-                  coordinates: geojson
-                }
-              }
-            },
-            layout: {
-              'line-join': 'round',
-              'line-cap': 'round'
-            },
-            paint: {
-              'line-color': '#3887be',
-              'line-width': 5,
-              'line-opacity': 0.75
-            }
-          });
-        }
-        // add turn instructions here at the end
-      };
-      req.send();
-      console.log("get route is running")
-    }
+    //   // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    //   var req = new XMLHttpRequest();
+    //   req.open('GET', url, true);
+    //   req.onload = function() {
+    //     var json = JSON.parse(req.response);
+    //     console.log(json);
+    //     var data = json.routes[0];
+    //     var route = data.geometry.coordinates;
+    //     var geojson = {
+    //       type: 'Feature',
+    //       properties: {},
+    //       geometry: {
+    //         type: 'LineString',
+    //         coordinates: route
+    //       }
+    //     };
+    //     // if the route already exists on the map, reset it using setData
+    //     if (map.getSource('route')) {
+    //       map.getSource('route').setData(geojson);
+    //     } else { // otherwise, make a new request
+    //       map.addLayer({
+    //         id: 'route',
+    //         type: 'line',
+    //         source: {
+    //           type: 'geojson',
+    //           data: {
+    //             type: 'Feature',
+    //             properties: {},
+    //             geometry: {
+    //               type: 'LineString',
+    //               coordinates: geojson
+    //             }
+    //           }
+    //         },
+    //         layout: {
+    //           'line-join': 'round',
+    //           'line-cap': 'round'
+    //         },
+    //         paint: {
+    //           'line-color': '#3887be',
+    //           'line-width': 5,
+    //           'line-opacity': 0.75
+    //         }
+    //       });
+    //     }
+    //     // add turn instructions here at the end
+    //   };
+    //   req.send();
+    //   console.log("get route is running")
+    // }
 
-    map.on('load', function() {
-      // make an initial directions request that
-      // starts and ends at the same location
-      var end = [13.4004488, 52.5314138];  // When I add this it "works", which means that the code below is not in the same scope as the code about with 'start' defined
-      getRoute(end);
+    // map.on('load', function() {
+    //   // make an initial directions request that
+    //   // starts and ends at the same location
+    //   var end = [13.4004488, 52.5314138];  // When I add this it "works", which means that the code below is not in the same scope as the code about with 'start' defined
+    //   getRoute(end);
 
-      // Add starting point to the map
-      map.addLayer({
-        id: 'point',
-        type: 'circle',
-        source: {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: [{
-              type: 'Feature',
-              properties: {},
-              geometry: {
-                type: 'Point',
-                coordinates: end
-              }
-            }
-            ]
-          }
-        },
-        paint: {
-          'circle-radius': 10,
-          'circle-color': '#266778'
-        }
-      });
-      // this is where the code from the next step will go
-    });
+    //   // Add starting point to the map
+    //   map.addLayer({
+    //     id: 'point',
+    //     type: 'circle',
+    //     source: {
+    //       type: 'geojson',
+    //       data: {
+    //         type: 'FeatureCollection',
+    //         features: [{
+    //           type: 'Feature',
+    //           properties: {},
+    //           geometry: {
+    //             type: 'Point',
+    //             coordinates: end
+    //           }
+    //         }
+    //         ]
+    //       }
+    //     },
+    //     paint: {
+    //       'circle-radius': 10,
+    //       'circle-color': '#266778'
+    //     }
+    //   });
+    //   // this is where the code from the next step will go
+    // });
 
   }
 };
