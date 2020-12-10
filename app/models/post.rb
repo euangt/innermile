d0d6e5a476efa8917e67738844ee3e2eb9571c43
Yoko Kasai <ykookaasi@gmail.com>
2020-12-10 20:52:06 +0100

    Change code order in posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,15 +9,15 @@ class Post < ApplicationRecord
 
   include PgSearch::Model
 
-  def new?
-    created_at > Time.now.ago(1.year)
-  end
-
   def business_name
     business.name
   end
 
   multisearchable against: [:content, :business_name], if: :new?
+
+  def new?
+    created_at > Time.now.ago(1.year)
+  end
 
   def self.rebuild_pg_search_documents
     find_each { |record| record.update_pg_search_document }
